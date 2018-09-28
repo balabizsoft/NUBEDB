109896e3739a97e1bd7e393d83d7bfc425c3b092
nube_bala <nube_bala@NUBE>
2018-09-28 17:44:18 +0800

    Subs Check

--- TestApp02/Program.cs
+++ TestApp02/Program.cs
@@ -13,7 +13,7 @@ namespace TestApp02
         static nubev2Entities db = new nubev2Entities();
         static void Main(string[] args)
         {
-            MemberInitScanByBankCode(24);
+            MemberInitScanByBankCode(47);
         }
 
 
@@ -50,153 +50,136 @@ namespace TestApp02
                 if (bank != null)
                 {
                     AppLib.WriteLog("Bank Id: {0}, Bank Code : {1}, Bank Name : {2}",bank.BANK_CODE, bank.BANK_USERCODE, bank.BANK_NAME);
-
-                    var lstMember = bank.MASTERMEMBERs.Select(x => new { x.MEMBER_CODE, x.MEMBER_ID, x.MEMBER_NAME,x.ICNO_NEW,x.ICNO_OLD, x.DATEOFJOINING, x.DATEOFBIRTH, x.REJOINED, MemberMonthEndStatus = x.MemberMonthEndStatus.FirstOrDefault() }).ToList();
-
-                    AppLib.WriteLog("No of Members: {0} [CLERICAL: {1} [ New Join: {2}, Re-Join: {3}], NON-CLERICAL: {4} [ New Join: {5}, Re-Join: {6}]]", lstMember.Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 1).Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 1 && x.REJOINED == 0).Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 1 && x.REJOINED == 1).Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 2).Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 2 && x.REJOINED == 0).Count(), lstMember.Where(x => x.MemberMonthEndStatus.MEMBERTYPE_CODE == 2 && x.REJOINED == 1).Count());
-                    AppLib.WriteLog("");
-                    foreach(var mType in lstMember.GroupBy(x=> x.MemberMonthEndStatus.MEMBERTYPE_CODE).ToList())
+                    AppLib.DisplayClear();
+                    int i = 1;
+                    AppLib.WriteLog("{0,5} {1,-10} {2,-10} {3,-50} {4,-15} {5,-15} {6,-10} {7,-10} {8,-10} {9,-10} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10} {15,-10} {16,-10} {17,-10} {18,-10} {19,-10} {20,-10} {21,-10} {22,-10} {23,-10} {24,-10}", "SNo", "Code", "MemberNo", "Member Name", "IC Old", "IC New", "DOB", "DOJ", "MEND", "LPAID", "SUBSAMT", "TSUBSAMT", "ACCSUBS", "SUBSDUE", "BFAMT", "TBFAMT", "ACCBF", "BFDUE", "TOTMNTS", "TOTMNTSPAD", "TOTMNTSDUE", "STATUSCDE", "RESIGNED", "STRUCKOFF", "STATUS");
+                    foreach (var mm in bank.MASTERMEMBERs.OrderBy(x=> x.DATEOFJOINING).ToList())
                     {
-                        foreach(var mmList in mType.GroupBy(x=> x.REJOINED).ToList())
+                        AppLib.DisplayMsg(1,1, string.Format( "[ {0,10} - {1} ]", i, bank.MASTERMEMBERs.Count()));
+                        
+                        var Status = mm.MemberMonthEndStatus.OrderBy(x=> x.StatusMonth).FirstOrDefault();
+
+                        decimal Subs = Status.MEMBERTYPE_CODE == 1 ? (mm.REJOINED == 0 ? 8 : 32) : (mm.REJOINED == 0 ? 4 : 16);
+                        decimal BFs = 3;
+
+                        string statusMsg = "";
+                        if (mm.DATEOFJOINING >= new DateTime(2005, 9, 1))
                         {
-                            
-                            int i = 1;
-                            AppLib.WriteLog("");
-                            AppLib.WriteLog("{0}Join {1}CLERICAL",mmList.Key==0? "New " : "Re-",mType.Key==1?"":"NON-" );
-                            AppLib.WriteLog("");
-                            AppLib.WriteLog("{0,5} {1,-10} {2,-10} {3,-50} {4,-15} {5,-15} {6,-10} {7,-10} {8,-10} {9,-10} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10} {15,-10} {16,-10} {17,-10} {18,-10} {19,-10} {20,-10} {21,-10} {22,-10} {23,-10} {24,-10}", "SNo", "Code", "MemberNo", "Member Name", "IC Old", "IC New", "DOB", "DOJ", "MEND", "LPAID", "SUBSAMT", "TSUBSAMT", "ACCSUBS", "SUBSDUE", "BFAMT", "TBFAMT", "ACCBF", "BFDUE", "TOTMNTS", "TOTMNTSPAD", "TOTMNTSDUE", "STATUSCDE", "RESIGNED", "STRUCKOFF", "STATUS");
 
-                            foreach (var mm in mmList)
+                            if (Status.SUBSCRIPTION_AMOUNT != Subs)
                             {
-                                Console.Clear();
-                                Console.WriteLine("Type Code : {0}", mType.Key);
-                                Console.WriteLine("Rejoin : {0}", mmList.Key);
-                                Console.WriteLine("Member Code: {0}", mm.MEMBER_CODE);
-                                var Status = mm.MemberMonthEndStatus;
-                                string statusMsg = "";
-                                if (mm.DATEOFJOINING >= new DateTime(2005, 9, 1))
-                                {
-                                    if (Status.SUBSCRIPTION_AMOUNT != 8)
-                                    {
-                                        statusMsg += "SUBSCRIPTION_AMOUNT MISMATCH, ";
-                                    }
-                                    if (Status.TOTALSUBCRP_AMOUNT != 8)
-                                    {
-                                        statusMsg += "TOTALSUBCRP_AMOUNT MISMATCH, ";
-                                    }
-                                    if (Status.ACCSUBSCRIPTION != 8)
-                                    {
-                                        statusMsg += "ACCSUBSCRIPTION MISMATCH, ";
-                                    }
-                                    if (Status.SUBSCRIPTIONDUE != 0)
-                                    {
-                                        statusMsg += "SUBSCRIPTIONDUE MISMATCH, ";
-                                    }
+                                statusMsg += "SUBSCRIPTION_AMOUNT MISMATCH, ";
+                            }
+                            if (Status.TOTALSUBCRP_AMOUNT != Subs)
+                            {
+                                statusMsg += "TOTALSUBCRP_AMOUNT MISMATCH, ";
+                            }
+                            if (Status.ACCSUBSCRIPTION != Subs)
+                            {
+                                statusMsg += "ACCSUBSCRIPTION MISMATCH, ";
+                            }
+                            if (Status.SUBSCRIPTIONDUE != 0)
+                            {
+                                statusMsg += "SUBSCRIPTIONDUE MISMATCH, ";
+                            }
 
-                                    if (Status.BF_AMOUNT != 3)
-                                    {
-                                        statusMsg += "BF_AMOUNT MISMATCH, ";
-                                    }
-                                    if (Status.TOTALBF_AMOUNT != 3)
-                                    {
-                                        statusMsg += "TOTALBF_AMOUNT MISMATCH, ";
-                                    }
-                                    if (Status.ACCBF != 3)
-                                    {
-                                        statusMsg += "ACCBF MISMATCH, ";
-                                    }
-                                    if (Status.BFDUE != 0)
-                                    {
-                                        statusMsg += "BFDUE MISMATCH, ";
-                                    }
+                            if (Status.BF_AMOUNT != BFs)
+                            {
+                                statusMsg += "BF_AMOUNT MISMATCH, ";
+                            }
+                            if (Status.TOTALBF_AMOUNT != BFs)
+                            {
+                                statusMsg += "TOTALBF_AMOUNT MISMATCH, ";
+                            }
+                            if (Status.ACCBF != BFs)
+                            {
+                                statusMsg += "ACCBF MISMATCH, ";
+                            }
+                            if (Status.BFDUE != 0)
+                            {
+                                statusMsg += "BFDUE MISMATCH, ";
+                            }
 
-                                    if (Status.TOTAL_MONTHS != 1)
-                                    {
-                                        statusMsg += "TOTAL_MONTHS MISMATCH, ";
-                                    }
-                                    if (Status.TOTALMONTHSPAID != 1)
-                                    {
-                                        statusMsg += "TOTALMONTHSPAID MISMATCH, ";
-                                    }
-                                    if (Status.TOTALMONTHSDUE != 0)
-                                    {
-                                        statusMsg += "TOTALMONTHSDUE MISMATCH, ";
-                                    }
+                            if (Status.TOTAL_MONTHS != 1)
+                            {
+                                statusMsg += "TOTAL_MONTHS MISMATCH, ";
+                            }
+                            if (Status.TOTALMONTHSPAID != 1)
+                            {
+                                statusMsg += "TOTALMONTHSPAID MISMATCH, ";
+                            }
+                            if (Status.TOTALMONTHSDUE != 0)
+                            {
+                                statusMsg += "TOTALMONTHSDUE MISMATCH, ";
+                            }
 
-                                    if (Status.STATUS_CODE != 1 && Status.STATUS_CODE != 5)
-                                    {
-                                        statusMsg += "Status Coce Mismatch, ";
-                                    }
+                            if (Status.STATUS_CODE != 1 && Status.STATUS_CODE != 5)
+                            {
+                                statusMsg += "Status Coce Mismatch, ";
+                            }
 
-                                    if (Status.STRUCKOFF == 1)
-                                    {
-                                        statusMsg += "Struckoff Mismatch, ";
-                                    }
+                            if (Status.STRUCKOFF == 1)
+                            {
+                                statusMsg += "Struckoff Mismatch, ";
+                            }
 
-                                    if (Status.RESIGNED == 1)
-                                    {
-                                        statusMsg += "Resigned Mismatch, ";
-                                    }
+                            if (Status.RESIGNED == 1)
+                            {
+                                statusMsg += "Resigned Mismatch, ";
+                            }
 
-                                    if (mm.DATEOFJOINING.Value.Year != Status.LASTPAYMENTDATE.Value.Year || mm.DATEOFJOINING.Value.Month != Status.LASTPAYMENTDATE.Value.Month)
-                                    {
-                                        statusMsg += "Last payment Date Mismatch, ";
-                                    }
+                            if (mm.DATEOFJOINING.Value.Year != Status.LASTPAYMENTDATE.Value.Year || mm.DATEOFJOINING.Value.Month != Status.LASTPAYMENTDATE.Value.Month)
+                            {
+                                statusMsg += "Last payment Date Mismatch, ";
+                            }
 
-                                    if (mm.DATEOFJOINING.Value.Year != Status.StatusMonth.Value.Year || mm.DATEOFJOINING.Value.Month != Status.StatusMonth.Value.Month)
-                                    {
-                                        statusMsg += "Status Month Date Mismatch, ";
-                                    }
-                                }
-                                else
-                                {
-                                    if (Status.StatusMonth.Value.Year != 2005 || Status.StatusMonth.Value.Month != 09)
-                                    {
-                                        statusMsg += "Status Month Date Mismatch, ";
-                                    }
-                                    var ts = DateTimeSpan.CompareDates(mm.DATEOFJOINING.Value, new DateTime(2005, 9, 1));
-                                    if ((Status.TOTALMONTHSPAID + Status.TOTALMONTHSDUE) != ((ts.Years * 12) + ts.Months) + 2)
-                                    {
-                                        statusMsg += "Total Month Mismatch, ";
-                                    }
-
-                                    if (Status.ACCSUBSCRIPTION != (Status.SUBSCRIPTION_AMOUNT * Status.TOTALMONTHSPAID))
-                                    {
-                                        statusMsg += "ACCSUBSCRIPTION MISMATCH, ";
-                                    }
-
-                                    if (Status.SUBSCRIPTIONDUE != (Status.SUBSCRIPTION_AMOUNT * Status.TOTALMONTHSDUE))
-                                    {
-                                        statusMsg += "SUBSCRIPTIONDUE MISMATCH, ";
-                                    }
-
-                                    if (Status.ACCBF != (Status.TOTALMONTHSPAID * 3) || Status.BFDUE != (Status.TOTALMONTHSDUE * 3))
-                                    {
-                                        statusMsg += "BF Mismatch, ";
-                                    }
-                                }
-
-                                if (!string.IsNullOrWhiteSpace(statusMsg))
-                                {
-                                    try
-                                    {
-                                        MemberMonthEndErrorLog d = new MemberMonthEndErrorLog()
-                                        {
-                                            Member_Code = mm.MEMBER_CODE,
-                                            YY = Status.StatusMonth.Value.Year,
-                                            MM = Status.StatusMonth.Value.Month,
-                                            ErrorMsg = "Member_Init" + statusMsg
-                                        };
-                                    }
-                                    catch (Exception ex) { AppLib.WriteLog(ex); }
-                                    statusMsg = " Error=> " + statusMsg;
-                                }
-
-                                AppLib.WriteLog("{0,5} {1,-10} {2,-10} {3,-50} {4,-15} {5,-15} {6:dd/MM/yyyy} {7:dd/MM/yyyy} {8:dd/MM/yyyy} {9:dd/MM/yyyy} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10} {15,-10} {16,-10} {17,-10} {18,-10} {19,-10} {20,-10} {21,-10} {22,-10} {23,-10} {24,-10}", i++, mm.MEMBER_CODE, mm.MEMBER_ID, mm.MEMBER_NAME, mm.ICNO_OLD, mm.ICNO_NEW, mm.DATEOFBIRTH, mm.DATEOFJOINING, Status.LASTPAYMENTDATE, Status.StatusMonth, Status.SUBSCRIPTION_AMOUNT, Status.TOTALSUBCRP_AMOUNT, Status.ACCSUBSCRIPTION, Status.SUBSCRIPTIONDUE, Status.BF_AMOUNT, Status.TOTALBF_AMOUNT, Status.ACCBF, Status.BFDUE, Status.TOTAL_MONTHS, Status.TOTALMONTHSPAID, Status.TOTALMONTHSPAID, Status.STATUS_CODE, Status.RESIGNED, Status.STRUCKOFF, statusMsg);
-
+                            if (mm.DATEOFJOINING.Value.Year != Status.StatusMonth.Value.Year || mm.DATEOFJOINING.Value.Month != Status.StatusMonth.Value.Month)
+                            {
+                                statusMsg += "Status Month Date Mismatch, ";
                             }
                         }
-                    }                   
+                        else
+                        {
+                            if (Status.StatusMonth.Value.Year != 2005 || Status.StatusMonth.Value.Month != 09)
+                            {
+                                statusMsg += "Status Month Date Mismatch, ";
+                            }
+                            var ts = DateTimeSpan.CompareDates(mm.DATEOFJOINING.Value, new DateTime(2005, 9, 1));
+                            if ((Status.TOTALMONTHSPAID + Status.TOTALMONTHSDUE) != ((ts.Years * 12) + ts.Months) + 2)
+                            {
+                                statusMsg += "Total Month Mismatch, ";
+                            }
+                            
+                            if (Status.ACCBF != (Status.TOTALMONTHSPAID * BFs))
+                            {
+                                statusMsg += "Acc BF Mismatch, ";
+                            }
+
+                            if (Status.BFDUE != (Status.TOTALMONTHSDUE * BFs))
+                            {
+                                statusMsg += "Due BF Mismatch, ";
+                            }
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(statusMsg))
+                        {
+                            try
+                            {
+                                MemberMonthEndErrorLog d = new MemberMonthEndErrorLog()
+                                {
+                                    Member_Code = mm.MEMBER_CODE,
+                                    YY = Status.StatusMonth.Value.Year,
+                                    MM = Status.StatusMonth.Value.Month,
+                                    ErrorMsg = "Member_Init" + statusMsg
+                                };
+                            }
+                            catch (Exception ex) { AppLib.WriteLog(ex); }
+                            statusMsg = " Error=> " + statusMsg;
+                        }
+
+                        AppLib.WriteLog("{0,5} {1,-10} {2,-10} {3,-50} {4,-15} {5,-15} {6:dd/MM/yyyy} {7:dd/MM/yyyy} {8:dd/MM/yyyy} {9:dd/MM/yyyy} {10,-10} {11,-10} {12,-10} {13,-10} {14,-10} {15,-10} {16,-10} {17,-10} {18,-10} {19,-10} {20,-10} {21,-10} {22,-10} {23,-10} {24,-10}", i++, mm.MEMBER_CODE, mm.MEMBER_ID, mm.MEMBER_NAME, mm.ICNO_OLD, mm.ICNO_NEW, mm.DATEOFBIRTH, mm.DATEOFJOINING, Status.LASTPAYMENTDATE, Status.StatusMonth, Status.SUBSCRIPTION_AMOUNT, Status.TOTALSUBCRP_AMOUNT, Status.ACCSUBSCRIPTION, Status.SUBSCRIPTIONDUE, Status.BF_AMOUNT, Status.TOTALBF_AMOUNT, Status.ACCBF, Status.BFDUE, Status.TOTAL_MONTHS, Status.TOTALMONTHSPAID, Status.TOTALMONTHSPAID, Status.STATUS_CODE, Status.RESIGNED, Status.STRUCKOFF, statusMsg);
+                    }
+                
                 }
                 else
                 {
